01a905b9d88e87a6bdffd6894f8b4a4a83b4a8e3
jatellezs <jatellez@miuandes.cl>
2019-05-10 23:03:57 -0400

    few changes

--- commandsapp/db/seeds.rb
+++ commandsapp/db/seeds.rb
@@ -12,3 +12,15 @@ posts = Post.create([{"title": "jaja salpica", "creation_date": "nil", "descript
 {"title": "jaja salpica", "creation_date": "nil", "description": "y salpicó po", "location": "Santiago, Chile", "open": "1", "solved": "0", "user": users.last }])
 blacklist = Blacklist.create([{date: "nil", user: users.first}])
 dumpster = Dumpster.create([{post: posts.first, date: "nil"}])
+geofence = Geofence.create([{location: "Santiago"}])
+administrator = Administrator.create([{last_access: "nil", superadmin: "1", geofence: geofence.first, user: users.last}])
+profiles = Profile.create([{picture: "nil", biography: "la bio", city: "Santiago", country: "Chile", user: users.first}, 
+	{picture: "nil", biography: "la otra bio", city: "Santiago", country: "Chile", user: users.last}])
+comments = Comment.create([{user: users.first, post: posts.first, content: "hago comentario"},
+	{user: users.last, post: posts.first, content: "hago comentario 2"}, 
+	{user: users.first, post: posts.last, content: "hago comentario de nuevo"}, 
+	{user: users.last, post: posts.last, content: "hago comentario muchos"}])
+votes = Vote.create([{user: users.first, post: posts.first, value: 1},
+	{user: users.first, post: posts.last, value: -1},
+	{user: users.last, post: posts.first, value: 1},
+	{user: users.last, post: posts.last, value: 1}])
